80ac1d35a366253fdc5e7b50693ac523e95e62d2
ansu <ansu.rajesh@gmail.com>
2021-07-28 05:50:14 +0530

    Added First  custom hook

--- src/App.js
+++ src/App.js
@@ -1,45 +1,31 @@
-import React, { useState , useEffect} from 'react';
+import React, { useState } from 'react';
 import ReactAutocomplete from 'react-autocomplete';
-import axios from 'axios';
+import {useSearch} from './hooks';
+
 function App() {
-   const [value, setValue] = useState();
-   const [items, setItems] = useState([]);
-
-   useEffect(() => {
-           axios.get(`https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${value}`)
-          .then(function(response){
-                const parsedResponse = [];
-                for(let i =0;i< response.data[1].length;i++){
-                    parsedResponse.push({
-                            id:response.data[3][i],
-                            label:response.data[1][i]
-                        })
-                }
-                setItems(parsedResponse);
-             })
-             .catch(function(error){
-                    console.log(error);
-                    debugger;
-             })
-
-   },[value]);
+   const [value, setValue] = useState('');
+   const { articles , status, error} = useSearch(value);
   return (
-        <ReactAutocomplete
-                items={items}
-                shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
-                getItemValue={item => item.label}
-                renderItem={(item, highlighted) =>
-                  <div
-                    key={item.id}
-                    style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
-                  >
-                    {item.label}
-                  </div>
-                }
-                value={value}
-                onChange={e => setValue(e.target.value )}
-                onSelect={value => setValue(value)}
-              />
+        <>
+            <p>  Status: {status} </p>
+            <p> Error : {error} </p>
+            <ReactAutocomplete
+                    items={articles}
+                    shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+                    getItemValue={item => item.label}
+                    renderItem={(item, highlighted) =>
+                      <div
+                        key={item.id}
+                        style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
+                      >
+                        {item.label}
+                      </div>
+                    }
+                    value={value}
+                    onChange={e => setValue(e.target.value )}
+                    onSelect={value => setValue(value)}
+                  />
+             </>
   )
 }
 
